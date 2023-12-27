90447e2d8a9275f7e9f9ba44ffe70a67086c0d53
Minh Hodler <ptminh2013@gmail.com>
2023-12-26 23:02:00 -0800

    test commit

---
+++ week0/min-max/min-max.py
@@ -0,0 +1,48 @@
+from static_array import *
+
+def min_max(arr: StaticArray) -> tuple[int, int]:
+    """
+    TODO: Write this implementation
+    """
+    pass
+
+# ------------------- TESTING -----------------------------------------
+if __name__ == "__main__":
+
+    print('\n# min_max example 1')
+    arr = StaticArray(5)
+    for i, value in enumerate([7, 8, 6, -5, 4]):
+        arr[i] = value
+    print(arr)
+    result = min_max(arr)
+    if result:
+        print(f"Min: {result[0]: 3}, Max: {result[1]}")
+    else:
+        print("min_max() not yet implemented")
+
+    print('\n# min_max example 2')
+    arr = StaticArray(1)
+    arr[0] = 100
+    print(arr)
+    result = min_max(arr)
+    if result:
+        print(f"Min: {result[0]}, Max: {result[1]}")
+    else:
+        print("min_max() not yet implemented")
+
+    print('\n# min_max example 3')
+    test_cases = (
+        [3, 3, 3],
+        [-10, -30, -5, 0, -10],
+        [25, 50, 0, 10],
+    )
+    for case in test_cases:
+        arr = StaticArray(len(case))
+        for i, value in enumerate(case):
+            arr[i] = value
+        print(arr)
+        result = min_max(arr)
+        if result:
+            print(f"Min: {result[0]: 3}, Max: {result[1]}")
+        else:
+            print("min_max() not yet implemented")
